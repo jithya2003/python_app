ef1eccfaf7c3eead753618fb08748845ed03b7bd
Ubuntu <azureuser@devSecOpsProject.xeky0a2htnzezmiz3a01dktoad.dx.internal.cloudapp.net>
2025-11-03 03:27:52 +0000

    Changed the python file

--- python_app.py
+++ python_app.py
@@ -3,7 +3,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Python App - CI/CD is success and Successfully deployed!"
+    return """
+    <div style="
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        font-size: 40px;
+        font-family: Arial, sans-serif;
+        color: #ffffff;
+        background: linear-gradient(135deg, #ff0000, #000000);
+        text-align: center;
+    ">
+        ✅ CI/CD SUCCESS! <br>🚀 Python App Deployed Successfully!
+    </div>
+    """
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
